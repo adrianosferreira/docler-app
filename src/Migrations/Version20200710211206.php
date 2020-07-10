ecf54ef5059a5b4d0cf58d22ae4cfc7605306c69
Adriano Ferreira <adrianokta@gmail.com>
2020-07-10 18:15:53 -0300

    fixup

---
+++ src/Migrations/Version20200710211206.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200710211206 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $this->addSql('ALTER TABLE tasks MODIFY COLUMN description TEXT NULL');
+        $this->addSql('ALTER TABLE tasks MODIFY COLUMN status int NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
